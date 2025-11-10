46a133ba64a25070c4381ff4c9213a70d4eadb18
ElPesi <elsanti.dominici@gmail.com>
2025-11-10 08:22:08 -0300

    M

--- APP/urls.py
+++ APP/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.main, name='main'),
+    path('', views.main, name='main'),
     path('services/', views.services, name='services'),
     path('budget/', views.budget, name='budget'),
     path('cotization/', views.cotization, name='cotization'),
-    path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- APP/views.py
+++ APP/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.contrib.auth import logout
 
 def main(request):
     return render(request, 'main.html')
@@ -13,8 +16,42 @@ def budget(request):
 def cotization(request):
     return render(request, 'cotization.html')
 
+
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, "Inicio de sesión exitoso.")
+            return redirect('main')
+        else:
+            messages.error(request, "Credenciales inválidas.")
+            return redirect('login')
     return render(request, 'login.html')
 
+
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El usuario ya existe.")
+            return redirect('signup')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, "Usuario creado correctamente. Ahora inicia sesión.")
+        return redirect('login')
+
     return render(request, 'signup.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
