6e5339f1b85f0ed91cfee42d2fac9be16aa4575b
ElPesi <elsanti.dominici@gmail.com>
2025-11-10 08:52:56 -0300

    arreglos

--- APP/views.py
+++ APP/views.py
@@ -35,22 +35,21 @@ def login(request):
 
 
 def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
 
         if User.objects.filter(username=username).exists():
-            messages.error(request, "El usuario ya existe.")
-            return redirect('signup')
+            messages.error(request, "El nombre de usuario ya existe.")
+            return render(request, "signup.html")
 
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
+        messages.success(request, "Cuenta creada con éxito. Ahora puedes iniciar sesión.")
+        return redirect("login")
 
-        messages.success(request, "Usuario creado correctamente. Ahora inicia sesión.")
-        return redirect('login')
-
-    return render(request, 'signup.html')
+    return render(request, "signup.html")
 
 def logout_view(request):
     logout(request)
